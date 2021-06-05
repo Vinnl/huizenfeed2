d04c8391fd541b96b0f86eebf591726e5c753020
Vincent <git@vincenttunru.com>
2021-06-05 21:29:30 +0200

    Add Peek&Pompe makelaars

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,13 @@ const feedConfigs: FeedConfig[] = [
     title: "Paul Thijssen makelaars - aanbod",
   },
   {
+    url: "https://www.peekenpompe.nl/nl/woningaanbod?ignoreType[]=isBought#{%22view%22:%22grid%22,%22sort%22:%22addedDesc%22,%22address%22:%22%22,%22title%22:%22%22,%22salesRentals%22:%22sales%22,%22salesPriceMin%22:0,%22salesPriceMax%22:400000,%22devSalesPriceMin%22:0,%22devSalesPriceMax%22:9999999999,%22rentalsPriceMin%22:0,%22rentalsPriceMax%22:9999999999,%22devRentalsPriceMin%22:0,%22devRentalsPriceMax%22:9999999999,%22surfaceMin%22:0,%22surfaceMax%22:9999999999,%22unitsMin%22:0,%22unitsMax%22:9999999999,%22devSurfaceMin%22:0,%22devSurfaceMax%22:9999999999,%22plotSurfaceMin%22:0,%22plotSurfaceMax%22:9999999999,%22roomsMin%22:2,%22roomsMax%22:9999999999,%22bedroomsMin%22:0,%22bedroomsMax%22:9999999999,%22bathroomsMin%22:0,%22bathroomsMax%22:9999999999,%22city%22:[%22Utrecht%22],%22district%22:[],%22mainType%22:[],%22buildType%22:[],%22tag%22:[],%22country%22:[],%22state%22:[],%22listingsType%22:[],%22ignoreType%22:[%22isBought%22],%22categories%22:[],%22status%22:%22available%22,%22statusStrict%22:false,%22user%22:%22%22,%22branch%22:%22%22,%22archiveTime%22:15778463,%22page%22:1}",
+    entrySelector: ".card-object",
+    titleSelector: ".card-object-address",
+    linkSelector: ".card-object-address",
+    title: "Peek&Pompe makelaars - aanbod",
+  },
+  {
     url: "https://moib.nl/aanbod/",
     entrySelector: ".horizon",
     titleSelector: "h3",
